d5d23684fe928b58c91b93cf3f59f0449b0ffeb2
Ryan D. Lewis <ryan.lewis@anl.gov>
2024-09-05 15:21:42 -0500

    pushing

---
+++ applications/test_app/protocols/ot2_1.py
@@ -0,0 +1,44 @@
+from opentrons import protocol_api
+
+# metadata
+metadata = {
+    "protocolName": "Demo protocol 1",
+    "author": "Name <opentrons@example.com>",
+    "description": "Demo protocol 1",
+}
+
+# requirements
+requirements = {"robotType": "OT-2", "apiLevel": "2.19"}
+
+# protocol run function
+def run(protocol: protocol_api.ProtocolContext):
+    # labware
+    plate = protocol.load_labware(
+        "corning_96_wellplate_360ul_flat", location="1"
+    )
+    reservoir = protocol.load_labware(
+        "nest_12_reservoir_15ml", location="10"
+    )
+    tiprack = protocol.load_labware(
+        "opentrons_96_tiprack_300ul", location="11"
+    )
+    tiprack.set_offset(x=-0.7, y=0.3, z=0.0)
+
+    # pipettes
+    left_pipette = protocol.load_instrument(
+        "p300_single_gen2", mount="left", tip_racks=[tiprack]
+    )
+
+    # commands
+    left_pipette.pick_up_tip(tiprack["A1"])
+
+    left_pipette.aspirate(100, reservoir["A1"])
+    left_pipette.dispense(100, plate["A1"])
+
+    left_pipette.aspirate(100, reservoir["A1"])
+    left_pipette.dispense(100, plate["A2"])
+
+    left_pipette.aspirate(100, reservoir["A1"])
+    left_pipette.dispense(100, plate["A3"])
+
+    left_pipette.drop_tip()
--- applications/test_app/src/test_app.py
+++ applications/test_app/src/test_app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Test experiment application that uses the WEI client to run test workflow."""
 
-import json
 from pathlib import Path
 
 from wei import ExperimentClient
@@ -13,22 +12,15 @@ def main() -> None:
     exp = ExperimentClient("localhost", "8000", "Test_Experiment")
 
     # The path to the Workflow definition yaml file
-    wf_path = Path(__file__).parent.parent / "workflows" / "test_arm.workflow.yaml"
+    wf_path = Path(__file__).parent.parent / "workflows" / "test.workflow.yaml"
 
     # This runs the workflow
-    run_info = exp.start_run(
-        wf_path.resolve(),
-        payload={},
-    )
-    print(json.dumps(run_info, indent=2))
-
-    # The below line can be used to fetch the result and save it in our local directory
-    # exp.get_wf_result_file(
-    #     run_id=run_info["run_id"],
-    #     filename=run_info["hist"]["Take Picture"]["action_msg"],
-    #     output_filepath="experiment_output.jpg",
-    # )
-
+    for i in range(1, 4):
+        exp.start_run(
+            wf_path.resolve(),
+            payload={"ot2_protocol": f"/home/rpl/biomedal_workcell/applications/test_app/protocols/ot2_{i}.py"},
+            blocking=True,
+        )
 
 if __name__ == "__main__":
     main()
