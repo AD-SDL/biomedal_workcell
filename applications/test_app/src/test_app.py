045a1d9f8d9b97023574a2922ad16ffc3893dc1b
Casey Stone <cstone@anl.gov>
2024-11-18 12:38:58 -0600

    file dump for autoprotocol experiment

--- applications/test_app/src/test_app.py
+++ applications/test_app/src/test_app.py
@@ -12,15 +12,15 @@ def main() -> None:
     exp = ExperimentClient("localhost", "8000", "Test_Experiment")
 
     # The path to the Workflow definition yaml file
-    wf_path = Path(__file__).parent.parent / "workflows" / "test.workflow.yaml"
+    wf_path = Path(__file__).parent.parent / "workflows" / "workflow_pcr.yaml"
 
     # This runs the workflow
-    for i in range(1, 4):
-        exp.start_run(
-            wf_path.resolve(),
-            payload={"ot2_protocol": f"/home/rpl/biomedal_workcell/applications/test_app/protocols/ot2_{i}.py"},
-            blocking=True,
-        )
+    
+    exp.start_run(
+        wf_path.resolve(),
+        payload={"ot2_protocol": "/home/rpl/biomedal_workcell/applications/test_app/protocols/liquid_protocol_pcr.yaml"},
+        blocking=True,
+    )
 
 if __name__ == "__main__":
     main()
