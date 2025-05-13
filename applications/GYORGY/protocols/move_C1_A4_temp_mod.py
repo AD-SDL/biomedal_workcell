f5320117b073368b4e6e7df01797d236d2c513f1
abestroka <abestroka@gmail.com>
2025-05-13 12:09:25 -0500

    updated pd app workflow

---
+++ applications/GYORGY/protocols/move_C1_A4_temp_mod.py
@@ -0,0 +1,43 @@
+requirements = {"robotType": "Flex"}
+
+from opentrons import protocol_api
+
+
+metadata = {
+    "protocolName": "Load move temp module plate to A4",
+    "author": "Abe astroka@anl.gov",
+    "description": "pd cfpe assay",
+    "apiLevel": "2.18"
+}
+
+def run(protocol: protocol_api.ProtocolContext):
+
+    deck = {}
+    pipettes = {}
+
+
+    ################
+    # load labware #
+    ################
+    # deck["A4"] = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", "A4")
+
+    module = protocol.load_module("temperature module gen2", "C1")
+    deck["C1"] = module.load_labware("nest_96_wellplate_100ul_pcr_full_skirt")
+
+    # deck["C1"].set_offset(x=1.0, y=1.6, z=11.0)
+    # adapter = module.load_adapter("opentrons_96_flat_bottom_adapter")
+
+    deck["A1"] = protocol.load_trash_bin("A1")
+
+    pipettes["right"] = protocol.load_instrument("flex_8channel_50", "right", tip_racks=[])
+
+    pipettes["left"] = protocol.load_instrument("flex_1channel_1000", "left", tip_racks=[])
+
+
+    ####################
+    # execute commands #
+    ####################
+
+    # move_test
+    # module.open_labware_latch()
+    protocol.move_labware(labware=deck["C1"], new_location="A4", use_gripper=True)
--- applications/GYORGY/src/PD_app.py
+++ applications/GYORGY/src/PD_app.py
@@ -39,8 +39,11 @@ def main() -> None:
     protocol_directory = app_directory / "protocols"
 
     # # workflow paths (run instruments)
-    # run_hidex_wf = wf_run_instrument_directory / "run_hidex.yaml"
+    run_hidex_wf = wf_run_instrument_directory / "run_hidex.yaml"
     run_flex_wf = wf_run_instrument_directory / "run_flex.yaml"
+    run_sealer_wf = wf_run_instrument_directory / "run_sealer.yaml"
+    run_peeler_wf = wf_run_instrument_directory / "run_peeler.yaml"
+
 
     # # workflow paths (set up and tear down related)
 
@@ -48,13 +51,18 @@ def main() -> None:
     remove_lid_move_to_flex = wf_transfers_directory / "remove_lid_move_to_flex.yaml"
     flex_to_hidex_wf = wf_transfers_directory / "flex_to_hidex_wf.yaml"
     hidex_to_flex_wf = wf_transfers_directory / "hidex_to_flex_wf.yaml"
+    flex_to_sealer_wf = wf_transfers_directory / "flex_to_sealer_wf.yaml"
+    sealer_to_hidex_wf = wf_transfers_directory / "sealer_to_hidex_wf.yaml"
+    hidex_to_peeler_wf = wf_transfers_directory / "hidex_to_peeler_wf.yaml"
+    peeler_to_flex_wf = wf_transfers_directory / "peeler_to_flex_wf.yaml"
 
     # protocol paths (for OT-Flex)
-    add_enzyme_protocol = protocol_directory / "pd_cfpe_multi.yaml"
-    add_substrate_protocol = protocol_directory / "pd_cfpe_multi_2.yaml"
-    move_to_staging_protocol = protocol_directory / "move_to_staging_B1_A4.yaml"
+    add_enzyme_protocol = protocol_directory / "pd_cfpe_final.yaml"
+    add_substrate_protocol = protocol_directory / "pd_cfpe_final_2.yaml"
+    move_from_temp_to_staging_protocol = protocol_directory / "move_C1_A4_temp_mod.py"
     move_to_staging_2_protocol = protocol_directory / "move_to_staging_D1_A4.yaml"
-    move_from_staging_protocol = protocol_directory / "move_from_staging_A4_D1.yaml"
+    move_from_staging_to_temp_protocol = protocol_directory / "move_A4_C1_temp_mod.py"
+
 
     # important variables
 
@@ -82,7 +90,7 @@ def main() -> None:
         simulate=False,
     )
 
-    payload = {"current_flex_protocol": str(move_to_staging_protocol)}
+    payload = {"current_flex_protocol": str(move_from_temp_to_staging_protocol)}
 
     # add fluorescence and enzyme
     experiment_client.start_run(
@@ -92,13 +100,13 @@ def main() -> None:
         simulate=False,
     )
 
-    #     #move from flex to hidex  WORKING!
-    experiment_client.start_run(
-        flex_to_hidex_wf.resolve(),
-        payload=payload,
-        blocking=True,
-        simulate=False,
-    )
+    # #move from flex to hidex
+    # experiment_client.start_run(
+    #     flex_to_hidex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
 
     #     #run hidex to detect fluorescence
     #     experiment_client.start_run(
@@ -108,17 +116,66 @@ def main() -> None:
     #     simulate=False,
     # )
 
-    #     #move from hidex to flex   WORKING!
+    # move from flex to sealer
     experiment_client.start_run(
-        hidex_to_flex_wf.resolve(),
+        flex_to_sealer_wf.resolve(),
         payload=payload,
         blocking=True,
         simulate=False,
     )
 
-    payload = {"current_flex_protocol": str(move_from_staging_protocol)}
+    # seal
+    experiment_client.start_run(
+        run_sealer_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
 
-    # add fluorescence and enzyme
+    # sealer to hidex
+    experiment_client.start_run(
+        sealer_to_hidex_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
+
+    # run hidex incubate #TODO: specific file name needed in payload
+    experiment_client.start_run(
+        run_hidex_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
+
+    # hidex to peeler
+    experiment_client.start_run(
+        hidex_to_peeler_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
+
+    # run peeler
+    experiment_client.start_run(
+        run_peeler_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
+
+    # peeler to flex staging
+    experiment_client.start_run(
+        peeler_to_flex_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
+
+
+    # flex staging to temp block
+    payload = {"current_flex_protocol": str(move_from_staging_to_temp_protocol)}
+
     experiment_client.start_run(
         run_flex_wf.resolve(),
         payload=payload,
@@ -126,33 +183,53 @@ def main() -> None:
         simulate=False,
     )
 
-    payload = {"current_flex_protocol": str(add_substrate_protocol)}
 
-    #add substrate
-    experiment_client.start_run(
-        run_flex_wf.resolve(),
-        payload=payload,
-        blocking=True,
-        simulate=False,
-    )
 
-    payload = {"current_flex_protocol": str(move_to_staging_2_protocol)}
+    # #     #move from hidex to flex 
+    # experiment_client.start_run(
+    #     hidex_to_flex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
 
-    # add fluorescence and enzyme
-    experiment_client.start_run(
-        run_flex_wf.resolve(),
-        payload=payload,
-        blocking=True,
-        simulate=False,
-    )
+    # payload = {"current_flex_protocol": str(move_from_staging_protocol)}
 
-    #flex to hidex
-    experiment_client.start_run(
-    flex_to_hidex_wf.resolve(),
-    payload=payload,
-    blocking=True,
-    simulate=False,
-)
+#     # add fluorescence and enzyme
+#     experiment_client.start_run(
+#         run_flex_wf.resolve(),
+#         payload=payload,
+#         blocking=True,
+#         simulate=False,
+#     )
+
+#     payload = {"current_flex_protocol": str(add_substrate_protocol)}
+
+#     #add substrate
+#     experiment_client.start_run(
+#         run_flex_wf.resolve(),
+#         payload=payload,
+#         blocking=True,
+#         simulate=False,
+#     )
+
+#     payload = {"current_flex_protocol": str(move_to_staging_2_protocol)}
+
+#     # add fluorescence and enzyme
+#     experiment_client.start_run(
+#         run_flex_wf.resolve(),
+#         payload=payload,
+#         blocking=True,
+#         simulate=False,
+#     )
+
+#     #flex to hidex
+#     experiment_client.start_run(
+#     flex_to_hidex_wf.resolve(),
+#     payload=payload,
+#     blocking=True,
+#     simulate=False,
+# )
 #     #hidex run?
 #     experiment_client.start_run(
 #     run_hidex_wf.resolve(),
