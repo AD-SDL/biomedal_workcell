e589c3353022453c486fc37bc8d81630417653a2
Casey Stone <cstone@anl.gov>
2025-07-15 16:22:11 -0500

    working transfers minus thermocycler

--- applications/golden_gate/src/golden_gate.py
+++ applications/golden_gate/src/golden_gate.py
@@ -55,7 +55,7 @@ def main() -> None:
 
     # protocol paths (for OT-Flex)
     run_gg = protocol_directory / "gg_flex.py"
-    move_to_staging_protocol = protocol_directory / "move_to_staging_B2_A4.yaml"
+    move_to_staging_protocol = protocol_directory / "move_to_staging_B2_A4.py"
     move_from_staging_protocol = protocol_directory / "move_from_staging_A4_B2.yaml"
     #TODO: possibly break up in future when running multiple plates, ie make large quantity of master mix and use repeatedly
 
@@ -68,12 +68,12 @@ def main() -> None:
     # TODO: transfer plate into Flex: move to exchange (from where?), remove lid, move to flex
 
     #run golden gate experiement on flex
-    experiment_client.start_run(
-        run_flex_wf.resolve(),
-        payload=payload,
-        blocking=True,
-        simulate=False,
-    )
+    # experiment_client.start_run(
+    #     run_flex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
 
     payload = {"current_flex_protocol": str(move_to_staging_protocol)}
 
@@ -85,7 +85,7 @@ def main() -> None:
         simulate=False,
     )
 
-    # move from flex to sealer
+    # move from flex to sealer to thermo
     experiment_client.start_run(
         flex_to_thermocycler_wf.resolve(),
         payload=payload,
@@ -111,17 +111,17 @@ def main() -> None:
         simulate=False,
     )
 
-    payload = {"current_flex_protocol": str(move_from_staging_protocol)}
+    # payload = {"current_flex_protocol": str(move_from_staging_protocol)}
 
-    #move from flex staging A back to flex B2
-    experiment_client.start_run(
-        run_flex_wf.resolve(),
-        payload=payload,
-        blocking=True,
-        simulate=False,
-    )
+    # #move from flex staging A back to flex B2
+    # experiment_client.start_run(
+    #     run_flex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
 
-    
+
 
 if __name__ == "__main__":
     main()
