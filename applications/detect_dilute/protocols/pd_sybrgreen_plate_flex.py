2470882123b045f7962f6252d4dfdc4a0d5a723b
abestroka <abestroka@gmail.com>
2025-07-25 13:45:33 -0500

    detect and dilute outlined

---
+++ applications/detect_dilute/protocols/pd_sybrgreen_plate_flex.py
@@ -0,0 +1,151 @@
+from opentrons import protocol_api
+import itertools
+from opentrons.protocol_api import SINGLE
+
+
+metadata = {
+    'protocolName': 'Protein Design PCR',
+    'author': 'LDRD team ',
+    'description': 'PCR for Protein Design',
+    'source': 'FlexGB/pd_pcr_01.py'
+}
+
+requirements = {"robotType": "Flex", "apiLevel": "2.20"}
+
+
+# Protocol Configuration
+config = {
+    # Reagent mixing
+    'number_of_pcr_samples': 6,
+    'sybrgreen_volume': 198,
+    'pcr_sample_volume': 2,
+
+    # Master mix and reagent settings
+    'pcr_master_mix_well_volume': 100,
+    'water_volume': 20,
+    'pcr_master_mix_volume': 24,
+    'water_well': 1,
+    'master_mix_start_well': 48,
+    'sybrgreen_well': 3,
+
+
+    # Temperature settings
+    'temperature': 4,  # °C
+
+    # Labware
+    'source_plate_type': 'nest_96_wellplate_100ul_pcr_full_skirt',
+    'pcr_plate_type': 'nest_96_wellplate_100ul_pcr_full_skirt',
+    'gg_plate_type': 'nest_96_wellplate_100ul_pcr_full_skirt',
+    'reagent_plate_type': 'nest_12_reservoir_15ml',
+    'sybrgreen_plate_type': 'corning_96_wellplate_360ul_flat',
+    'tip_rack_type_50_01': 'opentrons_flex_96_tiprack_50ul',
+    'pipette_type_50': 'flex_8channel_50',
+    'tip_rack_type_200_01': 'opentrons_flex_96_tiprack_200ul',
+    'pipette_type_1000': 'flex_8channel_1000',
+
+    # Deck positions
+    'temp_module_position': 'C1',
+    'source_plate_initial_position': 'C1',
+    'gg_plate_position': 'B2',
+    'pcr_plate_position': 'B3',
+    'tip_rack_position_50_01': 'A1',
+    'tip_rack_position_200_01': 'A2',
+    'reagent_plate_position': 'D1',
+    'sybrgreen_plate_position': 'C2'
+}
+
+def sybrgreen_to_dest(protocol, reagent_plate, sybrgreen_plate, pipette, config):
+    sybrgreen_volume = config['sybrgreen_volume']
+    num_samples = config["number_of_pcr_samples"]
+    sybrgreen_well = reagent_plate.wells()[config['sybrgreen_well'] - 1]
+
+    for well in range(1, num_samples + 1):
+        dest_well = sybrgreen_plate.wells()[well - 1]
+        protocol.comment(f"\nTransferring to destination well {dest_well}:")
+        pipette.transfer(
+            sybrgreen_volume,
+            sybrgreen_well,
+            dest_well,
+            new_tip='always',  # Use fresh tip for each transfer
+        )
+
+def pcr_to_dest(protocol, pcr_plate, sybrgreen_plate, pipette, config):
+    pcr_sample_volume = config['pcr_sample_volume']
+    num_samples = config["number_of_pcr_samples"]
+
+    for well in range(1, num_samples + 1):
+        source_well = pcr_plate.wells()[well - 1]
+        dest_well = sybrgreen_plate.wells()[well - 1]
+        protocol.comment(f"\nTransferring to destination well {dest_well}:")
+        pipette.transfer(
+            pcr_sample_volume,
+            source_well,
+            dest_well,
+            new_tip='always',  # Use fresh tip for each transfer
+            mix_after = (3, 20)
+        )
+
+
+
+def run(protocol: protocol_api.ProtocolContext):
+    # Load temperature module and adapter
+    temp_mod = protocol.load_module(module_name="temperature module gen2", location=config['temp_module_position'])
+    temp_adapter = temp_mod.load_adapter("opentrons_96_well_aluminum_block")
+
+    # Set temperature
+    temp_mod.set_temperature(config['temperature']) #TODO: make seperate, or just set earlier, only 1 hour with plate
+
+    # Load source plate initially on A4
+    # source_plate = protocol.load_labware(config['source_plate_type'], config['source_plate_initial_position'])
+    source_plate = temp_adapter.load_labware(config['source_plate_type'])
+
+
+    chute = protocol.load_waste_chute()
+
+    source_plate.set_offset(x=0.40, y=0.50, z=2.40)
+
+
+    # Load destination plate
+    pcr_plate = protocol.load_labware(config['pcr_plate_type'], config['pcr_plate_position'])
+    pcr_plate.set_offset(x=0.4, y=0.4, z=0.0)
+
+    gg_plate = protocol.load_labware(config['gg_plate_type'], config['gg_plate_position'])
+    gg_plate.set_offset(x=0.4, y=0.4, z=0.0)
+
+    sybrgreen_plate = protocol.load_labware(config['sybrgreen_plate_type'], config['sybrgreen_plate_position'])
+
+    reagent_plate = protocol.load_labware(config['reagent_plate_type'], config['reagent_plate_position'])
+
+
+    tiprack_50 = protocol.load_labware(
+        load_name=config['tip_rack_type_50_01'], location=config['tip_rack_position_50_01']
+    )
+
+    # 8-channel P1000
+    tiprack_200 = protocol.load_labware(
+        load_name=config['tip_rack_type_200_01'], location=config['tip_rack_position_200_01']
+    )
+
+    # Pipettes
+    p50 = protocol.load_instrument('flex_8channel_50', mount='right', tip_racks=[tiprack_50])
+    p1000 = protocol.load_instrument('flex_8channel_1000', mount='left', tip_racks=[tiprack_200])
+
+    p50.configure_nozzle_layout(style=SINGLE, start='A1', tip_racks=[tiprack_50])
+    p1000.configure_nozzle_layout(style=SINGLE, start='A1', tip_racks=[tiprack_200])
+
+
+
+    # Perform combinatorial transfers #TODO: SWAP?  so adding small vols into large quant of master mix
+    sybrgreen_to_dest(protocol=protocol,
+                      reagent_plate=reagent_plate,
+                      sybrgreen_plate=sybrgreen_plate,
+                      pipette=p1000,
+                      config=config)
+    
+    pcr_to_dest(protocol=protocol,
+                pcr_plate=pcr_plate,
+                sybrgreen_plate=sybrgreen_plate,
+                pipette=p50,
+                config=config)
+
+
---
+++ applications/detect_dilute/src/detect_dilute.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+
+"""PCR Experiment Using Golden Gate Products"""
+
+from pathlib import Path
+
+from wei import ExperimentClient
+from wei.types.experiment_types import CampaignDesign, ExperimentDesign
+
+
+def main() -> None:
+    """Runs the Substrate Experiment Application"""
+
+    # INITIAL EXPERIMENT SETUP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # define the ExperimentDesign object that will be used to register the experiment
+    experiment_design = ExperimentDesign(
+        experiment_name="DD_Experiment",
+        experiment_description="DD Experiment",
+    )
+
+    # define a campaign object (useful if we want to group many of these substrate experiments together)
+    campaign = CampaignDesign(
+        campaign_name="ProteinDesign_Campaign",
+        campaign_description="Protein Design Campaign",
+    )
+
+    # define the experiment client object that will communicate with the WEI server
+    experiment_client = ExperimentClient(
+        server_host="localhost",
+        server_port="8000",
+        experiment=experiment_design,
+        campaign=campaign,
+    )
+
+    # DEFINING PATHS AND VARIABLES -------------------------------
+
+    # directory path(s)
+    app_directory = Path(__file__).parent.parent
+    wf_directory = app_directory / "workflows"
+    wf_run_instrument_directory = wf_directory / "run_instrument"
+    wf_transfers_directory = wf_directory / "transfers"
+    protocol_directory = app_directory / "protocols"
+
+    # workflow paths (run instruments)
+    run_thermocycler_wf = wf_run_instrument_directory / "run_thermocycler.yaml"
+    run_flex_wf = wf_run_instrument_directory / "run_flex.yaml"
+
+    # # workflow paths (set up and tear down related)
+
+    # # workflow paths (pf400 transfers)
+    remove_lid_move_to_flex = wf_transfers_directory / "remove_lid_move_to_flex.yaml"
+    flex_to_thermocycler_wf = wf_transfers_directory / "flex_to_thermo_wf.yaml"
+    thermocycler_to_flex_wf = wf_transfers_directory / "thermocycler_to_flex_wf.yaml"
+    exchange_to_peeler_to_flexA_wf = wf_transfers_directory / "exchange_peeler_flexA_wf.yaml"
+    flexA_peeler_flexA_wf = wf_transfers_directory / "flexA_peeler_flexA.yaml"
+    flexA_sealer_flexA_wf = wf_transfers_directory / "flexA_sealer_flexA_wf.yaml"
+
+    # protocol paths (for OT-Flex)
+    run_dd = protocol_directory / "pd_detect_dilute_flex.py"
+    move_source_to_staging_protocol = protocol_directory / "move_to_staging_C1_A4.py"
+    move_from_staging_B2_protocol = protocol_directory / "move_from_staging_A4_B2.py"
+    move_from_staging_C1_protocol = protocol_directory / "move_from_staging_A4_C1.py"
+    move_from_staging_B3_protocol = protocol_directory / "move_from_staging_A4_B3.py"
+    move_pcr_to_staging_protocol = protocol_directory / "move_pcr_to_staging.py"
+
+    #TODO: possibly break up in future when running multiple plates, ie make large quantity of master mix and use repeatedly
+
+    # important variables
+    payload = {"current_flex_protocol": str(run_dd)}
+
+
+    # EXPERIMENT STEPS: ----------------------------------------------
+
+
+    #assume pcr plate is thermocycled and on exchange, peel and return to B3
+
+    experiment_client.start_run(
+        exchange_to_peeler_to_flexA_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
+
+    payload = {"current_flex_protocol": str(move_from_staging_B3_protocol)}
+
+    experiment_client.start_run(
+        run_flex_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
+
+    # run d&d
+
+    payload = {"current_flex_protocol": str(run_dd)}
+
+    experiment_client.start_run(
+        run_flex_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
+
+    #optional: remove og source plate from temp block
+
+
+
+    #peel gg product, place in flex B2
+
+    # experiment_client.start_run(
+    #     exchange_to_peeler_to_flexA_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
+
+    # payload = {"current_flex_protocol": str(move_from_staging_B2_protocol)}
+
+    # experiment_client.start_run(
+    #     run_flex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
+
+    # #peel source plate, place back on C1
+
+    # payload = {"current_flex_protocol": str(move_source_to_staging_protocol)}
+
+    # experiment_client.start_run(
+    #     run_flex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
+
+    # experiment_client.start_run(
+    #     flexA_peeler_flexA_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
+
+
+    # payload = {"current_flex_protocol": str(move_from_staging_C1_protocol)}
+
+    # experiment_client.start_run(
+    #     run_flex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
+
+    # payload = {"current_flex_protocol": str(run_pcr)}
+    # #run pcr experiment
+    # experiment_client.start_run(
+    #     run_flex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
+
+    # #move pcr plate to staging, seal, thermocycle
+
+    # payload = {"current_flex_protocol": str(move_pcr_to_staging_protocol)}
+
+    # experiment_client.start_run(
+    #     run_flex_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
+
+    # experiment_client.start_run(
+    #     flex_to_thermocycler_wf.resolve(),
+    #     payload=payload,
+    #     blocking=True,
+    #     simulate=False,
+    # )
+
+    #peel pcr plate, return to flex
+
+
+    
+
+if __name__ == "__main__":
+    main()
