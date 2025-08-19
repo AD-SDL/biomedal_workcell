85727d02b78307c8cb5aef1ee39088efcdd28ff4
Casey Stone <cstone@anl.gov>
2025-08-19 16:08:51 -0500

    ot2bioalpha labware offsets

--- applications/Autoprotocol/liquid_protocol_2.py
+++ applications/Autoprotocol/liquid_protocol_2.py
@@ -4,9 +4,9 @@ from opentrons import protocol_api
 
 
 metadata = {
-    "protocolName": "NA",
+    "protocolName": "PriyankaTEST",
     "author": "Autoprotocol",
-    "description": "NA",
+    "description": "PriyankaTEST",
     "apiLevel": "2.12",
     "info": "A PCR protocol written by Autoprotocol",
     "name": "Test Protocol",
@@ -25,15 +25,15 @@ def run(protocol: protocol_api.ProtocolContext):
     module = protocol.load_module("Temperature Module", "3")
     deck["3"] = module.load_labware("nest_96_wellplate_100ul_pcr_full_skirt")
 
-    deck["3"].set_offset(x=1.0, y=1.6, z=11.0)
+    deck["3"].set_offset(x=1.0, y=1.4, z=5.7)
 
     deck["1"] = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", "1")
 
-    deck["1"].set_offset(x=0.0, y=1.7, z=3.9)
+    deck["1"].set_offset(x=1.2, y=-0.3, z=0.6)
 
     deck["7"] = protocol.load_labware("opentrons_96_tiprack_20ul", "7")
 
-    deck["7"].set_offset(x=0.2, y=0.4, z=0.0)
+    deck["7"].set_offset(x=0.2, y=1.6, z=-0.8)
 
     pipettes["left"] = protocol.load_instrument("p20_single_gen2", "left", tip_racks=[deck["7"]])
 
