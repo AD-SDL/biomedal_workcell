09225a9725d1bc6ae0a9256df2f6cf37219aa746
abestroka <abestroka@gmail.com>
2025-08-26 12:56:23 -0500

    81 reagent pcr protocol

--- applications/pcr/protocols/move_to_staging_B1_A4.py
+++ applications/pcr/protocols/move_to_staging_B1_A4.py
@@ -11,11 +11,11 @@ requirements = {"robotType": "Flex", "apiLevel": "2.20"}
 
 def run(protocol: protocol_api.ProtocolContext):
     # Labware
-    source_plate = protocol.load_labware('nest_96_wellplate_200ul_flat', 'B1')
+    # source_plate = protocol.load_labware('nest_96_wellplate_200ul_flat', 'B1')
     pcr_plate = protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', 'B2')
-    mastermix_tube = protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', 'C2') 
+    mastermix_tube = protocol.load_labware('nest_12_reservoir_15ml', 'B3') #TODO change
     water_reservoir = protocol.load_labware('nest_12_reservoir_15ml', 'A1')
-    temp_mod = protocol.load_module(module_name="temperature module gen2", location="C1")
+    temp_mod = protocol.load_module(module_name="temperature module gen2", location="B1")
     temp_adapter = temp_mod.load_adapter("opentrons_96_well_aluminum_block")
     dna = temp_adapter.load_labware('nest_96_wellplate_100ul_pcr_full_skirt')
 
@@ -31,14 +31,11 @@ def run(protocol: protocol_api.ProtocolContext):
 
     # Pipettes
     p50 = protocol.load_instrument('flex_8channel_50', mount='right', tip_racks=[tiprack_50])
-    p1000 = protocol.load_instrument('flex_8channel_1000', mount='left', tip_racks=[tiprack_200])
 
-    p50.configure_nozzle_layout(style=SINGLE, start='A1', tip_racks=[tiprack_50])
-    p1000.configure_nozzle_layout(style=SINGLE, start='A1', tip_racks=[tiprack_200])
 
 
     # load trash bin
     # _ = protocol.load_trash_bin("D1")
     chute = protocol.load_waste_chute()
 
-    protocol.move_labware(labware=mastermix_tube, new_location="A4", use_gripper=True)
+    protocol.move_labware(labware=dna, new_location="A4", use_gripper=True)
