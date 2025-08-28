b6ef13f537730483fa237405b19f1a8dd635fa1f
Casey Stone <cstone@anl.gov>
2025-08-28 16:20:36 -0500

    first working python startup script

--- startup_scripts/start_biometra_thermocycler_96.py
+++ startup_scripts/start_biometra_thermocycler_96.py
@@ -3,19 +3,15 @@ General Notes:
 - use the 'adding_python' branch of the biometra module 
 - use wei 0.6.2
 
-# TODO: Test
+# TODO: Write bash
+# NOTE: this python script works :) 
 
 """
 import subprocess
 
-# activate the environment
-result = subprocess.run(['.\source\repos\biometra_module\.venv\Scripts\activate'], shell=True, capture_output=True, text=True)
-print(result.stdout)
-
-# start the node
-result = subprocess.run(['python .\source\repos\biometra_module\src\biometra_rest_node.py, --host "0.0.0.0" --port 2008'], shell=True, capture_output=True, text=True)
-print(result.stdout)
-
-
+subprocess.Popen(
+    'start "Biometra TRobotII Thermocycler" cmd /K "C:\\Users\\RPL\\source\\repos\\biometra_module\\.venv\\Scripts\\activate & python C:\\Users\\RPL\\source\\repos\\biometra_module\\src\\biometra_rest_node.py --host "0.0.0.0" --port 2008"', 
+    shell=True
+)
 
 
