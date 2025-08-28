690787a90bde89e3688459ef8c177957a597cd23
abestroka <abestroka@gmail.com>
2025-08-28 11:03:00 -0500

    Update move_to_staging_C2_A4.py

--- applications/detect_dilute/protocols/move_to_staging_C2_A4.py
+++ applications/detect_dilute/protocols/move_to_staging_C2_A4.py
@@ -11,7 +11,7 @@ requirements = {"robotType": "Flex", "apiLevel": "2.20"}
 
 def run(protocol: protocol_api.ProtocolContext):
     # Labware
-    source_plate = protocol.load_labware('nest_96_wellplate_200ul_flat', 'B1')
+    # source_plate = protocol.load_labware('nest_96_wellplate_200ul_flat', 'B1')
     # pcr_plate = protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', 'C2')
     pcr_plate = protocol.load_labware('corning_96_wellplate_360ul_flat', 'C2')
     mastermix_tube = protocol.load_labware('nest_12_reservoir_15ml', 'B3') #TODO change
@@ -19,6 +19,9 @@ def run(protocol: protocol_api.ProtocolContext):
     temp_mod = protocol.load_module(module_name="temperature module gen2", location="C1")
     temp_adapter = temp_mod.load_adapter("opentrons_96_well_aluminum_block")
     dna = temp_adapter.load_labware('nest_96_wellplate_100ul_pcr_full_skirt')
+    temp_mod2 = protocol.load_module(module_name="temperature module gen2", location="B1")
+    temp_adapter2 = temp_mod2.load_adapter("opentrons_96_well_aluminum_block")
+    dna2 = temp_adapter2.load_labware('nest_96_wellplate_100ul_pcr_full_skirt')
 
     tiprack_50 = protocol.load_labware(
         load_name="opentrons_flex_96_tiprack_50ul", location="A2",
