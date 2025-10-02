5a06fd5c86d8f48e4d24391e83dde19d7039c41b
Casey Stone <cstone@anl.gov>
2025-10-02 15:22:29 -0500

    tested first workflow

--- applications/NIDHI/exp3_one_plate/src/experiment_app.py
+++ applications/NIDHI/exp3_one_plate/src/experiment_app.py
@@ -43,10 +43,9 @@ def main() -> None:
 
 
     # DEFINE PATHS AND VARIABLES ---------------------------------
-
     # capture the expriment ID
     experiment_id = experiment_client.experiment.experiment_id
-    experiment_label = "3a"
+    experiment_label = "3"
 
     # directory paths
     app_directory = Path(__file__).parent.parent   # experiment app
@@ -82,27 +81,58 @@ def main() -> None:
     test_prints = True  # if True, print out extra info for testing purposes
 
     # incubation_seconds_initial = 36000  # 10 hours
-    incubation_seconds_initial = 20 # for testing
+    incubation_seconds_initial = 20 # TESTING
     incubation_seconds_between_readings = 3600  # 1 hour
 
-    # initial payload setup
-    payload = {
-        "current_ot2_protocol": str(inoculate_protocol),
+    # # initial payload setup
+    # payload = {
+    #     "current_ot2_protocol": str(inoculate_protocol),
+    #     "ot2_new_plate_location": "ot2biobeta_deck1_wide",
+    #     "ot2_old_plate_location": "ot2biobeta_deck3_wide",
+    #     "ot2_safe_path": "safe_path_ot2biobeta",
+    #     "tip_box_location": 4,
+    #     "stack": "stack1",
+    #     "new_lid_location": "lidnest1",
+    #     "old_lid_location": "lidnest2",
+    #     "remove_lid_safe_path": "safe_path_lidnest_1",
+    #     "bmg_assay_name": "NIDHI",
+    #     "incubator_node": "inheco_devID2_floor0",
+    #     "incubator_location": "inheco_devID2_floor1_nest",
+    #     "incubation_seconds": incubation_seconds_initial,
+    # }
+
+    exp1_variables = {
+        "lid_location": "lidnest_2_wide",  # use old lid location at start
+        "safe_lid_location": "safe_path_lidnest_2",
+        "ot2_node": "ot2biobeta",
         "ot2_new_plate_location": "ot2biobeta_deck1_wide",
         "ot2_old_plate_location": "ot2biobeta_deck3_wide",
         "ot2_safe_path": "safe_path_ot2biobeta",
+        "incubator_node": "inheco_devID2_floor0",
+        "incubator_location": "inheco_devID2_floor0_nest",
         "tip_box_location": 4,
-        "stack": "stack1",
-        "new_lid_location": "lidnest1",
-        "old_lid_location": "lidnest2",
-        "remove_lid_safe_path": "safe_path_lidnest_1",
-        "bmg_assay_name": "NIDHI",
-        "incubator_node": "inheco_devID2_floor1",
-        "incubator_location": "inheco_devID2_floor1_nest",
-        "incubation_seconds": incubation_seconds_initial,
+        "new_stack": "stack1",
+        "trash_stack": "stack4",
     }
 
-    # EXPERIMENT LOOP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # initial payload setup  (experiment 1 focused at start)
+    payload = {
+        "lid_location": exp1_variables["lid_location"],
+        "safe_lid_location": exp1_variables["safe_lid_location"],
+        "ot2_node": exp1_variables["ot2_node"],
+        "ot2_location": exp1_variables["ot2_new_plate_location"],
+        "ot2_safe_path": exp1_variables["ot2_safe_path"],
+        "tip_box_location": exp1_variables["tip_box_location"],
+        "incubator_node": exp1_variables["incubator_node"],
+        "incubator_location": exp1_variables["incubator_location"],
+        "incubation_seconds": incubation_seconds_initial,
+        "current_ot2_protocol": None,
+        "use_existing_resources": False,
+        "bmg_assay_name": "NIDHI",
+    }
+
+
+    # # EXPERIMENT LOOP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     """
     Experiment setup at start:
@@ -112,28 +142,28 @@ def main() -> None:
         Stack 1: extra substrate plates with lids
         OT-2 (ot2biobeta) decks 4-11: tip racks
         ALL OTHER LOCATIONS: EMPTY
-
     """
-    # Move immediately into incubator with lid on for 10 hours  # NOT TESTED
-    experiment_client.start_run(
-        exchange_to_run_incubator_wf.resolve(),
-        payload=payload,
-        blocking=True,
-        simulate=False,
 
-    )
-
-    # # transfer plate 0 into bmg and take reading (plate0_T10)   # NOT TESTED
-    # timestamp_now = int(datetime.now().timestamp())
-    # payload["bmg_data_output_name"] = (
-    #     f"{experiment_label}_{timestamp_now}_{experiment_id}_exp1_{plate_num}_T{reading_in_plate_num}.txt"
-    # )
-    # run_info = experiment_client.start_run(
-    #     incubator_to_run_bmg_wf.resolve(),
+    # # Move immediately into incubator with lid on for 10 hours  # WORKS
+    # experiment_client.start_run(
+    #     exchange_to_run_incubator_wf.resolve(),
     #     payload=payload,
     #     blocking=True,
     #     simulate=False,
+
     # )
+
+    # transfer plate 0 into bmg and take reading (plate0_T10)   # NOT TESTED
+    timestamp_now = int(datetime.now().timestamp())
+    payload["bmg_data_output_name"] = (
+        f"{experiment_label}_{timestamp_now}_{experiment_id}_exp1_{plate_num}_T{reading_in_plate_num}.txt"
+    )
+    run_info = experiment_client.start_run(
+        incubator_to_run_bmg_wf.resolve(),
+        payload=payload,
+        blocking=True,
+        simulate=False,
+    )
     # # write utc bmg timestamp to csv data file
     # helper_functions.write_timestamps_to_csv(
     #     csv_directory_path=csv_data_directory,
@@ -142,7 +172,8 @@ def main() -> None:
     #     accurate_timestamp=run_info.steps[7].end_time,  # index 7 = bmg reading
     # )
     # if test_prints:
-    #     print(f"\twriting data to csv: {payload['bmg_data_output_name']}, with timestamp {run_info.steps[7].end_time}")
+    #     print(f"\twriting data to csv: {payload['bmg_data_output_name']}")
+    #     # print(f"\twriting data to csv: {payload['bmg_data_output_name']}, with timestamp {run_info.steps[7].end_time}")
 
 
     # # modify variables
